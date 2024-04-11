d75c1e3177e9daed3f61a78b4a9a74e00edea122
danieladebanjo55 <supremenftcoder@gmail.com>
2024-04-10 17:29:06 -0700

    docs: start screen upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Quiz from './Components/Quiz/Quiz'
+import React, { useState } from "react";
+import Quiz from "./Components/Quiz/Quiz";
+
+import logo from "../../quiz-website/src/assets/fhg.png";
 
 function App() {
+  const [begin, setBegin] = useState(true);
+
+  const startTest = () => {
+    setBegin(false);
+  };
+
   return (
     <div>
-      <Quiz />
+      {begin ? (
+        <>
+          <div className="container-main">
+            <img src={logo} alt="logo" className="logo" />
+            <h1>Welcome To Your FHG Training Exam</h1>
+            <h2 style={{ textAlign: "center" }}>
+              You're about to start Your Exam
+            </h2>
+            <button onClick={startTest}>Get Started</button>
+          </div>
+        </>
+      ) : (
+        <Quiz />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
